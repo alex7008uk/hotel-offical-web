78f0ce56206a0354829de5dbdbf442992a0330a8
User <alex7008uk@gmail.com>
2024-02-04 20:19:04 +0800

    update router path

--- src/global/router.js
+++ src/global/router.js
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
     element: <Nav />,
     children: [
       {
-        path: "/",
+        path: "/hotel-offical-web",
         element: <PageIntro />,
       },
       {
